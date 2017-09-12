964bfe36b4fecae306aead9c2f18825d5d500fb7
Gandlasaikiran <31859460+Gandlasaikiran@users.noreply.github.com>
2017-09-12 13:54:26 +0530

    two

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+		student s=new student();
+		s.student(int,string,float,double);
+		s.getId();
+		s.setId(int id);
+		s.getFullName();
+		s.setFullName(String fullName);
+		s.getBirthDate();
+		s.setBirthDate(Date birthDate);
+		s.getAvgMark();
+		s.setAvgMark(double avgMark) ;
+		s.hashCode() ;
+		s. equals(Object obj);
+		s. compareTo(Object o);
+	}
+
+}
